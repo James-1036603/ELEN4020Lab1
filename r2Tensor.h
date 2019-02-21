6a511621707a31ebc3cb3fe417cf65ab29d263f3
Unknown <1036603@students.wits.ac.za>
2019-02-21 20:01:50 +0200

    Added Classes

---
+++ r2Tensor.h
@@ -0,0 +1,16 @@
+#ifndef R2TENSOR_H
+#define R2TENSOR_H
+
+class r2Tensor
+{
+public:
+	r2Tensor()
+	{
+	}
+	~r2Tensor()
+	{
+	}
+
+};
+
+#endif // R2TENSOR_H
---
+++ r3Tensor.h
@@ -0,0 +1,16 @@
+#ifndef R3TENSOR_H
+#define R3TENSOR_H
+
+class r3Tensor
+{
+public:
+	r3Tensor()
+	{
+	}
+	~r3Tensor()
+	{
+	}
+
+};
+
+#endif // R3TENSOR_H
